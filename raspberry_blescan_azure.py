bf8fa12c10f27adc9c37c65119d2d170c9a37dce
luizfrborges <luizfrgorges@yahoo.com.br>
2020-11-23 15:53:40 -0300

    Eventhub key

--- raspberry_blescan_azure.py
+++ raspberry_blescan_azure.py
@@ -34,7 +34,7 @@ ADDRESS = "amqps://<mynamespace>.servicebus.windows.net/<myeventhub>"
 
 # SAS policy and key are not required if they are encoded in the URL
 USER = "RootManageSharedAccessKey"
-KEY = "<key>"
+KEY = "key"
 
 # ------------------------------------------------------------
 
